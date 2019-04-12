321eb1602281a4317f83f835ec072b39232b1e5a
TheFALSEGamer4 <37382895+TheFALSEGamer4@users.noreply.github.com>
2019-04-12 12:38:58 -0500

    Create tencodes.js

---
+++ tencodes.js
@@ -0,0 +1,314 @@
+function myFunction(){
+	var code = document.getElementById("code").value;
+	switch(code){
+		case "1":
+		  document.getElementById("text").innerHTML = "	Unable Copy - Change Location";
+		  break;
+		case "2":
+		  document.getElementById("text").innerHTML = "Signal Good";
+		  break;
+		case "3":
+		  document.getElementById("text").innerHTML = "Stop Transmitting";
+		  break;
+		case "4":
+		  document.getElementById("text").innerHTML = "Acknowledgment (OK)";
+		  break;
+		case "5":
+		  document.getElementById("text").innerHTML = "Relay";
+		  break;
+		case "6":
+		  document.getElementById("text").innerHTML = "Busy - Unless Urgent";
+		  break;
+		case "7":
+		  document.getElementById("text").innerHTML = "Out of Service";
+		  break;
+		case "8":
+		  document.getElementById("text").innerHTML = "In Service";
+		  break;
+		case "9":
+		  document.getElementById("text").innerHTML = "Repeat";
+		  break;
+		case "10":
+		  document.getElementById("text").innerHTML = "	Fight In Progress";
+		  break;
+		case "11":
+		  document.getElementById("text").innerHTML = "	Dog Case";
+		  break;
+		case "12":
+		  document.getElementById("text").innerHTML = "Stand By (Stop)";
+		  break;
+		case "13":
+		  document.getElementById("text").innerHTML = "Weather-Road Report";
+		  break;
+		case "14":
+		  document.getElementById("text").innerHTML = "Prowler Report";
+		  break;
+		case "15":
+		  document.getElementById("text").innerHTML = "Civil Disturbance";
+		  break;
+		case "16":
+		  document.getElementById("text").innerHTML = "Domestic Problem";
+		  break;
+		case "17":
+		  document.getElementById("text").innerHTML = "Meet Complainant";
+		  break;
+		case "18":
+		  document.getElementById("text").innerHTML = "Quickly";
+		  break;
+		case "19":
+		  document.getElementById("text").innerHTML = "Return to _____";
+		  break;
+		case "20":
+		  document.getElementById("text").innerHTML = "Location";
+		  break;
+		case "21":
+		  document.getElementById("text").innerHTML = "Call ( ) by Phone";
+		  break;
+		case "22":
+		  document.getElementById("text").innerHTML = "Disregard";
+		  break;
+		case "23":
+		  document.getElementById("text").innerHTML = "Arrived at Scene";
+		  break;
+		case "24":
+		  document.getElementById("text").innerHTML = "Assignment Completed";
+		  break;
+		case "25":
+		  document.getElementById("text").innerHTML = "Report in Person (Meet)";
+		  break;
+		case "26":
+		  document.getElementById("text").innerHTML = "Detaining Subject, Expedite";
+		  break;
+		case "27":
+		  document.getElementById("text").innerHTML = "(Drivers) License Information";
+		  break;
+		case "28":
+		  document.getElementById("text").innerHTML = "Vehicle Registration Information";
+		  break;
+		case "29":
+		  document.getElementById("text").innerHTML = "Check for Wanted";
+		  break;
+		case "30":
+		  document.getElementById("text").innerHTML = "Unnecessary Use of Radio";
+		  break;
+		case "31":
+		  document.getElementById("text").innerHTML = "Crime in Progress";
+		  break;
+		case "32":
+		  document.getElementById("text").innerHTML = "Man with Gun";
+		  break;
+		case "33":
+		  document.getElementById("text").innerHTML = "Emergency";
+		  break;
+		case "34":
+		  document.getElementById("text").innerHTML = "Riot";
+		  break;
+		case "35":
+		  document.getElementById("text").innerHTML = "Major Crime Alert";
+		  break;
+		case "36":
+		  document.getElementById("text").innerHTML = "Correct Time";
+		  break;
+		case "37":
+		  document.getElementById("text").innerHTML = "(Investigate) Suspicious Vehicle";
+		  break;
+		case "38":
+		  document.getElementById("text").innerHTML = "Stopping Suspicious Vehicle";
+		  break;
+		case "39":
+		  document.getElementById("text").innerHTML = "Urgent - Use Light, Siren";
+		  break;
+		case "40":
+		  document.getElementById("text").innerHTML = "Silent Run - No Light, Siren";
+		  break;
+		case "41":
+		  document.getElementById("text").innerHTML = "Beginning Tour of Duty";
+		  break;
+		case "42":
+		  document.getElementById("text").innerHTML = "Ending Tour of Duty";
+		  break;
+		case "43":
+		  document.getElementById("text").innerHTML = "Information";
+		  break;
+		case "44":
+		  document.getElementById("text").innerHTML = "Permission to Leave ___for___";
+		  break;
+		case "45":
+		  document.getElementById("text").innerHTML = "Animal Carcass at ___";
+		  break;
+		case "46":
+		  document.getElementById("text").innerHTML = "Assist Motorist";
+		  break;
+		case "47":
+		  document.getElementById("text").innerHTML = "Emergency Road Repair at ___";
+		  break;
+		case "48":
+		  document.getElementById("text").innerHTML = "Traffic Standard Repair at ___";
+		  break;
+		case "49":
+		  document.getElementById("text").innerHTML = "Traffic Light Out at ___";
+		  break;
+		case "50":
+		  document.getElementById("text").innerHTML = "Accident (F-Fire, PI-Personal Injury, PD-Property Damage)";
+		  break;
+		case "51":
+		  document.getElementById("text").innerHTML = "Wrecker Needed";
+		  break;
+		case "52":
+		  document.getElementById("text").innerHTML = "Ambulance Needed";
+		  break;
+		case "53":
+		  document.getElementById("text").innerHTML = "Road Blocked at ___";
+		  break;
+		case "54":
+		  document.getElementById("text").innerHTML = "Livestock on Highway";
+		  break;
+		case "55":
+		  document.getElementById("text").innerHTML = "Intoxicated Driver";
+		  break;
+		case "56":
+		  document.getElementById("text").innerHTML = "Intoxicated Pedestrian";
+		  break;
+		case "57":
+		  document.getElementById("text").innerHTML = "Hit and Run (F-Fire, PI-Personal Injury, PD-Property Damage)";
+		  break;
+		case "58":
+		  document.getElementById("text").innerHTML = "Direct Traffic";
+		  break;
+		case "59":
+		  document.getElementById("text").innerHTML = "Convoy or Escort";
+		  break;
+		case "60":
+		  document.getElementById("text").innerHTML = "Squad in Vicinity";
+		  break;
+		case "61":
+		  document.getElementById("text").innerHTML = "Personnel in Area";
+		  break;
+		case "62":
+		  document.getElementById("text").innerHTML = "Reply to Message";
+		  break;
+		case "63":
+		  document.getElementById("text").innerHTML = "Prepare Make Written Copy";
+		  break;
+		case "64":
+		  document.getElementById("text").innerHTML = "Message for Local Delivery";
+		  break;
+		case "65":
+		  document.getElementById("text").innerHTML = "Net Message Assignment";
+		  break;
+		case "66":
+		  document.getElementById("text").innerHTML = "Message Cancellation";
+		  break;
+		case "67":
+		  document.getElementById("text").innerHTML = "Clear for Net Message";
+		  break;
+		case "68":
+		  document.getElementById("text").innerHTML = "Dispatch Information";
+		  break;
+		case "69":
+		  document.getElementById("text").innerHTML = "Message Received";
+		  break;
+		case "70":
+		  document.getElementById("text").innerHTML = "Fire Alarm";
+		  break;
+		case "71":
+		  document.getElementById("text").innerHTML = "Advise Nature of Fire";
+		  break;
+		case "72":
+		  document.getElementById("text").innerHTML = "Report Progress on Fire";
+		  break;
+		case "73":
+		  document.getElementById("text").innerHTML = "Smoke Report";
+		  break;
+		case "74":
+		  document.getElementById("text").innerHTML = "Negative";
+		  break;
+		case "75":
+		  document.getElementById("text").innerHTML = "In Contact with ___";
+		  break;
+		case "76":
+		  document.getElementById("text").innerHTML = "En Route ___";
+		  break;
+		case "77":
+		  document.getElementById("text").innerHTML = "ETA (Estimated Time of Arrival)";
+		  break;
+		case "78":
+		  document.getElementById("text").innerHTML = "Need Assistance";
+		  break;
+		case "79":
+		  document.getElementById("text").innerHTML = "Notify Coroner";
+		  break;
+		case "80":
+		  document.getElementById("text").innerHTML = "Chase in Progress";
+		  break;
+		case "81":
+		  document.getElementById("text").innerHTML = "Breatherlizer Report";
+		  break;
+		case "82":
+		  document.getElementById("text").innerHTML = "Reserve Lodging";
+		  break;
+		case "83":
+		  document.getElementById("text").innerHTML = "Work School Crossing at ___";
+		  break;
+		case "84":
+		  document.getElementById("text").innerHTML = "If Meeting ___ Advise ETA (Estimated Time of Arrival)";
+		  break;
+		case "85":
+		  document.getElementById("text").innerHTML = "Delay due to ___";
+		  break;
+		case "86":
+		  document.getElementById("text").innerHTML = "Officer / Operator on Duty";
+		  break;
+		case "87":
+		  document.getElementById("text").innerHTML = "Pickup / Distribute Checks";
+		  break;
+		case "88":
+		  document.getElementById("text").innerHTML = "Present Telephone # of ___";
+		  break;
+		case "89":
+		  document.getElementById("text").innerHTML = "Bomb Threat";
+		  break;
+		case "90":
+		  document.getElementById("text").innerHTML = "Bank Alarm at ___";
+		  break;
+		case "91":
+		  document.getElementById("text").innerHTML = "Pick Up Prisoner / Subject";
+		  break;
+		case "92":
+		  document.getElementById("text").innerHTML = "Improperly Parked Vehicle";
+		  break;
+		case "93":
+		  document.getElementById("text").innerHTML = "Blockade";
+		  break;
+		case "94":
+		  document.getElementById("text").innerHTML = "Drag Racing";
+		  break;
+		case "95":
+		  document.getElementById("text").innerHTML = "Prisoner / Subject in Custody";
+		  break;
+		case "96":
+		  document.getElementById("text").innerHTML = "Mental Subject";
+		  break;
+		case "97":
+		  document.getElementById("text").innerHTML = "Check (Test) Signal";
+		  break;
+		case "98":
+		  document.getElementById("text").innerHTML = "Prison / Jail Break";
+		  break;
+		case "99":
+		  document.getElementById("text").innerHTML = "Wanted / Stolen Indicated";
+		  break;
+		case "101":
+		  document.getElementById("text").innerHTML = "What is Status? (Are you secure?)";
+		  break;
+		case "106":
+		  document.getElementById("text").innerHTML = "Secure (Status is secure)";
+		  break;
+
+		default:
+		  document.getElementById("text").innerHTML = "Invalid";
+		  break;
+		
+	}
+	
+}
