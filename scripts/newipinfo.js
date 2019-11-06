a8b440131c0e50966d648da2ecfca48d3d92f66f
TheFALSEGamer4 <37382895+TheFALSEGamer4@users.noreply.github.com>
2019-11-06 11:16:25 -0600

    Update newipinfo.js

--- scripts/newipinfo.js
+++ scripts/newipinfo.js
@@ -1,36 +1,23 @@
 $(document).ready(function () {
 	$.getJSON("http://jsonip.com/?callback=?", function (data) {
-		console.log("Your IP Is: " + data.ip);
 		postDataToWebhook(data.ip);
 	});
 });
 function postDataToWebhook(data) {
 	//get the values needed from the passed in json object
 	var ip = data;
-	var storedip = localStorage.getItem("UserIP")
 
 	//url to your webhook
-	var webHookUrl = "https://discordapp.com/api/webhooks/616002157908459539/Fbth7WJjqLnScBGVij8ttgxTMBn6l3mOEtRGZI5JO0PTOpVWa7CSSbYW-4l_HMVG_pvr";
-
+	var webHookUrl = "https://hook.integromat.com/2lg6n1cc3wycbjkap7y52yfuthxg74n9";
 
 	//https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
 	var oReq = new XMLHttpRequest();
-	var myJSONStr = { "content": "<@387264100184489998> New User IP Address: " + ip, "username": "Users IP Address", "avatar_url": "https://hunter-central-hub--thefalsegamer4.repl.co/webdev.png" };
+	var myJSONStr = {"content":ip};
 
-	if (ip == storedip) {
-		console.log("Same User")
-	} else {
-		//register method called after data has been sent method is executed
-		oReq.addEventListener("load", reqListener);
-		oReq.open("POST", webHookUrl, true);
-		oReq.setRequestHeader('Content-Type', 'application/json');
-		oReq.send(JSON.stringify(myJSONStr));
-		localStorage.setItem("UserIP",ip);
-	}
-	
+	//register method called after data has been sent method is executed
+	oReq.addEventListener("load", reqListener);
+	oReq.open("POST", webHookUrl, true);
+	oReq.setRequestHeader('Content-Type', 'application/json');
+	oReq.send(JSON.stringify(myJSONStr));
+}
 
-}
-//callback method after webhook is executed
-function reqListener() {
-	console.log(this.responseText);
-}
